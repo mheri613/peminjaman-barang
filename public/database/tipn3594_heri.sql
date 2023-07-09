bf923a970fbf56dbf212d899f8351c87fac859c5
Heri Maulana <mheri613@gmail.com>
2023-07-09 22:56:24 +0700

    Add files via upload

---
+++ public/database/tipn3594_heri.sql
@@ -0,0 +1,41 @@
+-- --------------------------------------------------------
+-- Host:                         heri.tipnl.com
+-- Server version:               10.5.20-MariaDB-cll-lve - MariaDB Server
+-- Server OS:                    Linux
+-- HeidiSQL Version:             12.5.0.6679
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table tipn3594_heri.failed_jobs: ~0 rows (approximately)
+
+-- Dumping data for table tipn3594_heri.migrations: ~5 rows (approximately)
+REPLACE INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+	(3, '2014_10_12_100000_create_password_resets_table', 1),
+	(4, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(5, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+
+-- Dumping data for table tipn3594_heri.password_resets: ~0 rows (approximately)
+
+-- Dumping data for table tipn3594_heri.password_reset_tokens: ~0 rows (approximately)
+
+-- Dumping data for table tipn3594_heri.personal_access_tokens: ~0 rows (approximately)
+
+-- Dumping data for table tipn3594_heri.users: ~1 rows (approximately)
+REPLACE INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+	(1, 'satu', 'satu@satu.com', NULL, '$2y$10$c17LM5oVd/41P9bGw7giXe9kSQ6D2CGUzSGjvaAgnqqwOBe/8gpYa', NULL, '2023-07-09 05:42:41', '2023-07-09 05:42:41');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
